be99792f442c7f566e93e15e10c6dbb3688690cf
kyrieCCC <1186624948@qq.com>
2022-11-16 12:02:00 +0800

    feat: 204&206-LIST

---
+++ 206.reverseList.js
@@ -0,0 +1,21 @@
+// 206. 反转链表
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 输入：head = [1,2,3,4,5]
+// 输出：[5, 4, 3, 2, 1]
+
+var reverseList = function (head) {
+    let pre = null
+    let cur = head
+    while (cur) {
+        const temp = cur.next //记录下来当前指针的next指向
+        cur.next = pre //在这一步进行改变指针指向的操作
+        pre = cur 
+        cur = temp  //这两步是把当前的指针指向往后移动一位，使用滑动窗口，滑动来改变指针的指向
+    }
+    return pre
+};
+//双指针操作，滑动改变指针的指向
+
+const head = [1, 2, 3, 4, 5]
+const result = reverseList()
+console.log(result)
