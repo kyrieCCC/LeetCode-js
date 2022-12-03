741135b3b8001b0ec745c78a625b0fd1f43aaacb
kyrieCCC <1186624948@qq.com>
2022-12-03 20:09:26 +0800

    feat: 461

---
+++ 400-500/461.hammingDistance .js
@@ -0,0 +1,17 @@
+
+var hammingDistance = function (x, y) { 
+    x = x.toString(2)
+    y = y.toString(2)
+    maxLen = Math.max(x.length, y.length)
+    x = x.padStart(maxLen, 0)
+    y = y.padStart(maxLen, 0)
+    let ans = 0
+    for (let i = 0; i < maxLen; i++){
+        if(x[i] !== y[i]) ans++
+    }
+    return ans
+}
+
+const x = 4, y = 1
+const res = hammingDistance(x, y)
+console.log(res)
