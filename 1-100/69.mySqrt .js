a06234cf8583a4368be29bf68e352459811165e9
kyrieCCC <1186624948@qq.com>
2022-12-07 22:48:15 +0800

    feat: 69

---
+++ 1-100/69.mySqrt .js
@@ -0,0 +1,30 @@
+// 69. x 的平方根
+// 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+// 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+// 输入：x = 8
+// 输出：2
+// 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+// 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+var mySqrt = function (x) { 
+    let left = 0, right = x
+    if (x === 1) {
+        return 1
+    }
+    let mid, res
+    while (1) {
+        mid = Math.floor((left + right) / 2)
+        if (mid ** 2 > x) {
+            right = mid
+        }
+        else if (mid ** 2 < x) {
+            left = mid
+        }
+        else if (mid ** 2 === x) {
+            return mid
+        }
+        res = Math.floor((left + right) / 2)
+        if(res === mid) return res
+    }
+}
+const x = 8
+console.log(mySqrt(x))
