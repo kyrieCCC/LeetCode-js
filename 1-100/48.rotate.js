1aa7dc4ca761c9ea3dae1acca5db701aef4bf2b5
kyrieCCC <1186624948@qq.com>
2022-12-07 12:14:11 +0800

    feat: 48?

---
+++ 1-100/48.rotate.js
@@ -0,0 +1,36 @@
+// 48. 旋转图像
+// 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+// 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+// 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// 输出：[[7,4,1],[8,5,2],[9,6,3]]
+var rotate1 = function (matrix) {
+    const len = matrix[0].length
+    const new_matrix = new Array(len).fill(0).map(() => new Array(len).fill(0))
+    for (let i = 0; i < len; i++){
+        for (let j = 0; j < len; j++){
+            new_matrix[j][len - i - 1] = matrix[i][j]
+        }
+    }
+    for (let i = 0; i < len; i++){
+        for (let j = 0; j < len; j++){
+            matrix[i][j] = new_matrix[i][j]
+        }
+    }
+    return matrix
+};
+// 利用新的矩阵来记录第一个矩阵里面的数字，但是时间复杂度为on²太高啦
+var rotate = function (matrix) {
+    const n = matrix[0].length
+    for (let i = 0; i < Math.floor(n / 2); i++){
+        for (let j = 0; j < Math.floor((n + 1) / 2); j++){
+            const temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i] 
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
+        }
+    }
+    return matrix
+};
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+console.log(rotate(matrix))
