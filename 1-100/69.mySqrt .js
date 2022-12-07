d32249701ab88d764fc5d5fe9630aa9f11172a8b
kyrieCCC <1186624948@qq.com>
2022-12-07 22:51:49 +0800

    feat: 69done

--- 1-100/69.mySqrt .js
+++ 1-100/69.mySqrt .js
@@ -8,23 +8,25 @@
 var mySqrt = function (x) { 
     let left = 0, right = x
     if (x === 1) {
-        return 1
+        return 1 //特殊值处理，因为1乘什么都都是1 
     }
     let mid, res
     while (1) {
         mid = Math.floor((left + right) / 2)
-        if (mid ** 2 > x) {
+        if (mid ** 2 > x) { //mid大于结果，right左移
             right = mid
         }
-        else if (mid ** 2 < x) {
+        else if (mid ** 2 < x) { //mid小于结果，left右移
             left = mid
         }
-        else if (mid ** 2 === x) {
+        else if (mid ** 2 === x) { //如果我们的结果不存在小数，那么这一项可以拿到结果
             return mid
         }
-        res = Math.floor((left + right) / 2)
+        res = Math.floor((left + right) / 2) //判断我们的小数位是否存在小数
         if(res === mid) return res
     }
 }
+//本题目使用的方法是二分法，通过left跟right来确定mid的范围
+//最后我们res是为了四舍五入，否则我们的万一我们的结果出现了小数位就会出现死循环的情况
 const x = 8
 console.log(mySqrt(x))
