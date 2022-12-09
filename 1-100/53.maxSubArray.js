5a5dcc73fcb790dd0a49ab182f69f873f19b5440
kyrieCCC <1186624948@qq.com>
2022-12-09 10:44:08 +0800

    feat: 53done

--- 1-100/53.maxSubArray.js
+++ 1-100/53.maxSubArray.js
@@ -31,6 +31,15 @@ var maxSubArrayTimeout = function(nums) {
 //很可惜的是多了一步，我们使用双循环的方法一个指向头部一个指向尾部，就可以拿到最大值
 //这个版本的答案是可行的，但是因为我们的事件复杂度太高，超时
 //新的解法如下
-
-const nums = [-2, 1]
+var maxSubArray = function (nums) { 
+    let pre = 0, max = nums[0]
+    nums.forEach(item => {
+        pre = Math.max(pre + item, item)
+        max = Math.max(max, pre)
+    });
+    return max
+}
+//动态规划的解法，使用两两比较 pre+item与item，但不能跨位置，这就符合了题目中描述的需要是连续字串
+//另外需要max的作用是max来比较当前的pre与item元素的值大小，可能某一个item就是最大的
+const nums = [-2, 1, -3, 1, -1, 2, 4, -5, 4]
 console.log(maxSubArray(nums))
