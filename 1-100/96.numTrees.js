f44d6a263c26d1f9b92917d8e34ffd3322d9def1
kyrieCCC <1186624948@qq.com>
2022-12-12 14:46:51 +0800

    feat: 96

---
+++ 1-100/96.numTrees.js
@@ -0,0 +1,17 @@
+// 96. 不同的二叉搜索树
+// 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？
+// 返回满足题意的二叉搜索树的种数。
+// 输入：n = 3
+// 输出：5 
+var numTrees = function (n) { 
+    const G = new Array(n + 1).fill(0)
+    G[0] = 1, G[1] = 1
+    for (let i = 2; i <= n; i++){
+        for (let j = 1; j <= i; j++){
+            G[i] += G[j - 1] * G[i - j]
+        }
+    }
+    return G[n]
+}
+const n = 3
+console.log(numTrees(n))
