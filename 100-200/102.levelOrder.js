9da5c4c25f53c609183b9e79f2e7d98c1413197c
kyrieCCC <1186624948@qq.com>
2022-12-15 11:31:05 +0800

    feat: 102done

--- 100-200/102.levelOrder.js
+++ 100-200/102.levelOrder.js
@@ -10,14 +10,16 @@ var levelOrder = function (root) {
     const tempArr = []
     tempArr.push(root)
     while (tempArr.length) {
-        const len = tempArr.length
-        res.push([])
-        for (let i = 1; i <= len; i++){
+        const len = tempArr.length//每次循环结束都记录长度，即这一层的节点数
+        res.push([])///开始记录新的一层节点
+        for (let i = 1; i <= len; i++){//把这一层的节点放进去，同时查看下一层的节点
             const node = tempArr.shift()
-            res[res.length - 1].push(node.val)
-            if (node.left) tempArr.push(node.left)
-            if(node.right) tempArr.push(node.right)
+            res[res.length - 1].push(node.val) //放入节点
+            if (node.left) tempArr.push(node.left)//寻找下一层的节点
+            if (node.right) tempArr.push(node.right)//寻找下一层的节点
         }
     }
     return res
 }
+//这里我们因为是层序遍历，我们需要返回每一层的结果，所以我们使用层序遍历
+//res每次循环都新增一个空数组，把这一层的全部节点都放到这个空数组当中
