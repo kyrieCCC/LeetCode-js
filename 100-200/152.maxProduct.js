a747a3b4d0f255b0524e3b29704af3db1c8da74c
kyrieCCC <1186624948@qq.com>
2023-01-05 20:36:10 +0800

    feat: 152detail

--- 100-200/152.maxProduct.js
+++ 100-200/152.maxProduct.js
@@ -9,14 +9,15 @@ var maxProduct = function(nums) {
     let min = nums[0], max = nums[0]
     let temp1 = 0, temp2 = 0
     for (let i = 1; i < nums.length; i++){
-        temp1 = min * nums[i]
+        temp1 = min * nums[i] //相乘计算结果
         temp2 = max * nums[i]
-        min = Math.min(temp1, temp2, nums[i])
-        max = Math.max(nums[i], temp2, temp1)
+        min = Math.min(temp1, temp2, nums[i]) //记录最小值
+        max = Math.max(nums[i], temp2, temp1) //记录最大值
         res = Math.max(max, res)
     }
     return res
 };
-
+//这个解法使用的是动态规划，我们每次相乘都会去比较最大跟最小值
+//为什么要比较最小值的原因是乘法会遇到负负得正的情况
 const nums = [2, 3, -2, 4]
 console.log(maxProduct(nums))
