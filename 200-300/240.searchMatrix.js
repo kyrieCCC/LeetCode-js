428d89257bb0c49c25f66138bafa65f1f2eb1d0d
kyrieCCC <1186624948@qq.com>
2023-01-11 20:10:50 +0800

    feat: 240new

--- 200-300/240.searchMatrix.js
+++ 200-300/240.searchMatrix.js
@@ -36,5 +36,21 @@ var searchMatrix2 = function(matrix, target) {
 //将二维数组的遍历简化为一维数组
 //时间复杂度为o（n+m），能够通过leetcode的全部用例
 
+var searchMatrix3 = function (matrix, target) {
+    let x = 0, y = matrix[0].length - 1
+    while (x < matrix.length && y >= 0) {
+        if (matrix[x][y] === target) {
+            return true
+        }
+        if (matrix[x][y] > target) {
+            y--
+        }
+        else {
+            x++
+        }
+    }
+    return false
+}
+
 const matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], target = 5
-console.log(searchMatrix(matrix, target))
+console.log(searchMatrix3(matrix, target))
