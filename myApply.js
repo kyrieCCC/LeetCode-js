deaf1bed9d011c7f07839076b2e6a5fe0d6f3658
kyrieCCC <1186624948@qq.com>
2023-01-13 21:48:50 +0800

    feat: myApplyDetail

--- myApply.js
+++ myApply.js
@@ -7,7 +7,7 @@ Function.prototype.myApply = function (obj, arr) {
     var obj = obj || window, args = []
     let result = ''
     obj.fn = this
-    if (Array.isArray(arr)) {
+    if (Array.isArray(arr)) { //判断是否是数组
         args = arr
         result = obj.fn(args)
         delete obj.fn
@@ -23,5 +23,9 @@ Function.prototype.myApply = function (obj, arr) {
         console.log('ERROR')
     }
 }
+
+//apply的原理其实与call类似，区别在于apply传入的第二个参数是一个参数数组
+//我们只需要根据这个参数数组对应的进行处理即可
+//大概的原理也就是将本来的this改变到myApply传入的对象上
 
 ppp.myApply({ x: 10 }, [1, 2, 3])
