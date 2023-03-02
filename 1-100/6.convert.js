a2807cc201cf6c86bdf669b81008cd14c93b8075
kyrieCCC <1186624948@qq.com>
2023-03-02 10:17:41 +0800

    feat: 6

--- 1-100/6.convert.js
+++ 1-100/6.convert.js
@@ -15,8 +15,11 @@ var convert = function (s, numRows) {
     if (r === 1 || n <= r) {
         return s
     }
+    //每次N排列的周期
     const time = 2 * r - 2
+    //排列需要的列数
     const cycle = Math.floor((n + time - 1) / time) * (r - 1)
+    //构建二维数组
     const mat = new Array(r).fill(0).map(() => new Array(cycle).fill(0))
     for (let i = 0, x = 0, y = 0; i < n; i++){
         mat[x][y] = s[i]
@@ -39,7 +42,10 @@ var convert = function (s, numRows) {
     }
     return res.join('')
 }
-
+//mihoyo春招
+//首先记录下每次N变换的周期，随后在计算出这个周期需要用到的列数
+//构建一个二维数组，这个数组是按N字排列好的
+//最后遍历这个二维数组，拿出除了0之外的数值即可
 
 
 const s = "PAYPALISHIRING", numRows = 3
