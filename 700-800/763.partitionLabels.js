33b737af971de25fac48c9567e7b39d207815963
kyrieCCC <1186624948@qq.com>
2023-03-02 23:08:36 +0800

    feat: 763done

--- 700-800/763.partitionLabels.js
+++ 700-800/763.partitionLabels.js
@@ -28,7 +28,15 @@ var partitionLabels = function(s) {
     }
     return res
 };
-//击败98%
+//击败98%,腾讯春招练习题
+//本题使用的是贪心算法
+// 在得到每个字母最后一次出现的下标位置之后，可以使用贪心的方法将字符串划分为尽可能多的片段，具体做法如下。
+// 从左到右遍历字符串，遍历的同时维护当前片段的开始下标 start\textit{start}start 和结束下标 end\textit{end}end，初始时 start=end=0\textit{start}=\textit{end}=0start=end=0。
+// 对于每个访问到的字母 ccc，得到当前字母的最后一次出现的下标位置 endc\textit{end}_cend c
+//  ，则当前片段的结束下标一定不会小于 endc\textit{end}_cendc
+//  ，因此令 end=max⁡(end,endc)\textit{end}=\max(\textit{end},\textit{end}_c)end=max(end,endc​)。
+// 当访问到下标 end\textit{end}end 时，当前片段访问结束，当前片段的下标范围是 [start,end][\textit{start},\textit{end}][start,end]，长度为 end−start+1\textit{end}-\textit{start}+1end−start+1，将当前片段的长度添加到返回值，然后令 start=end+1\textit{start}=\textit{end}+1start=end+1，继续寻找下一个片段。
+// 重复上述过程，直到遍历完字符串
 
 const s = "ababcbacadefegdehijhklij"
 console.log(partitionLabels(s));
