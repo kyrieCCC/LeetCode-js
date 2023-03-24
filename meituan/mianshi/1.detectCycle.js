5e6690f3db0df3e37c8c27db1f1a317f0d23527c
kyrieCCC <1186624948@qq.com>
2023-03-24 22:58:57 +0800

    feat: meituan-1-1

--- meituan/mianshi/1.detectCycle.js
+++ meituan/mianshi/1.detectCycle.js
@@ -1,4 +1,23 @@
 //判断链表是否有环,如果有环，返回入口节点
+//快慢指针
 const myFun = (head) => {
-    
+    let slow = head, fast = head
+    while (fast) {
+        slow = slow.next
+        if (fast.next) {
+            fast = fast.next.next
+        }
+        else {
+            return null
+        }
+        if (fast === slow) {
+            let node = head
+            while (node !== slow) {
+                node = node.next
+                slow = slow.next
+            }
+            return node
+        }
+    }
+    return null
 }
