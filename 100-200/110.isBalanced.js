aa8943101e3aa28230d45fc7489f80a42861633c
kyrieCCC <1186624948@qq.com>
2023-03-25 22:46:47 +0800

    feat: 110-2

--- 100-200/110.isBalanced.js
+++ 100-200/110.isBalanced.js
@@ -4,3 +4,26 @@
 // 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
 // 输入：root = [3,9,20,null,null,15,7]
 // 输出：true
+
+var isBalanced = function (root) { 
+    const dfs = (root) => {
+        if (!root) {
+            return 0
+        }
+        const left = dfs(root.left)
+        if (left == -1) {
+            return -1
+        }
+        const right = dfs(root.right)
+        if (right == -1) {
+            return -1
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1
+        }
+        else {
+            return 1 + Math.max(left, right)
+        }
+    }
+    return (dfs(root) != -1)
+}
