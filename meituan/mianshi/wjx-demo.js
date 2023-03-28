80b170471b963b42037c39d1437da7e4726defa3
kyrieCCC <1186624948@qq.com>
2023-03-28 22:03:15 +0800

    feat: meituan-4

---
+++ meituan/mianshi/wjx-demo.js
@@ -0,0 +1,22 @@
+//查找字符子串，子串的长度为10，且字串出现的次数不少于2次
+//例如
+//s = 'AAAAACCCCCAAAAACCCCCAAAAAGGGTTT'
+//输出 ['AAAAACCCCC', 'CCCCCAAAAA']
+
+const finSub = (str) => {
+    const map = new Map()
+    const res = []
+    for (let i = 0; i < str.length - 10; i++){
+        let subStr = str.slice(i, i + 10)
+        if (map.has(subStr)) {
+            res.push(subStr)
+        }
+        else {
+            map.set(subStr, 1)
+        }
+    }
+    return res
+}
+
+const s = 'AAAAACCCCCAAAAACCCCCAAAAAGGGTTT'
+console.log(finSub(s));
