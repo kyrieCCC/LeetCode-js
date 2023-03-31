f126211b3d06ccefdc17efcfee987b16f1a43d47
kyrieCCC <1186624948@qq.com>
2023-03-31 22:53:58 +0800

    feat: 美团二面算法解法一

--- meituan/mianshi/2mian/1.listSum.js
+++ meituan/mianshi/2mian/1.listSum.js
@@ -6,3 +6,30 @@
 // 将链表中的数字相加并使用逆序链表的方式返回
 // 返回5 -> 7 -> 9
 
+const listSum = (head1, head2) => {
+    const node = null, t = null
+    let cur = 0
+    while (head1 || head2) {
+        const item1 = head1 ? head1.val : 0
+        const item2 = head2 ? head2.val : 0
+        const sum = item1 + item2 + cur
+        if (!node) {
+            node = t = new ListNode(sum % 10)
+        }
+        else {
+            t.next = new ListNode(sum % 10)
+            t = t.next
+        }
+        cur = Math.floor(sum / 10)
+        if (head1) {
+            head1 = head1.next
+        }
+        if (head2) {
+            head2 = head2.next
+        }
+        if (cur > 0) {
+            t.next = new ListNode(cur)
+        }
+    }
+    return node
+}
