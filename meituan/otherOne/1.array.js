cc6d30a46c593a8e5a9d9856f49b1c9e429457b6
kyrieCCC <1186624948@qq.com>
2023-04-02 16:00:48 +0800

    feat: meituan-other-1-1

--- meituan/otherOne/1.array.js
+++ meituan/otherOne/1.array.js
@@ -9,3 +9,26 @@
 //   ]
 // 例如上面这个数组，根据其age重新排序
 
+
+const myFun = (arr) => {
+    const res = []
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 1; j < arr.length - i; j++){
+            if (arr[j].age < arr[j - 1].age) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+
+const arr = [
+        {name:'a',age:4},
+        {name:'b',age:6},
+        {name:'c',age:3},
+        {name:'d',age:2},
+        {name:'e',age:9},
+        {name:'f',age:8},
+]
+
+console.log(myFun(arr));
