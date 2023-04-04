46bb14d5e9018ca2f9e59d17c91f8ddb64224ee4
kyrieCCC <1186624948@qq.com>
2023-04-04 22:12:28 +0800

    feat: 118-1-2

--- 100-200/118.generate.js
+++ 100-200/118.generate.js
@@ -4,7 +4,19 @@
 // 输入: numRows = 5
 // 输出: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 
-
+//解法一 直接使用数学公式计算
 var generate = function (numRows) { 
+    const res = []
 
+    for (let i = 0; i < numRows; i++){
+        const row = new Array(i + 1).fill(1)
+        for (let j = 1; j < row.length - 1; j++){
+            row[j] = res[i - 1][j - 1] + res[i - 1][j]
+        }
+        res.push(row)
+    }
+    return res
 }
+
+const numRows = 5
+console.log(generate(numRows));
