5cb7e80633d6aad60e8b0db639826706ec71099d
kyrieCCC <1186624948@qq.com>
2023-04-04 22:28:39 +0800

    feat: 43-1-2

--- 1-100/43.multiply.js
+++ 1-100/43.multiply.js
@@ -7,7 +7,22 @@
 // 输入: num1 = "123", num2 = "456"
 // 输出: "56088"
 
-
-var multiply = function(num1, num2) {
-
+//使用 数学竖式
+var multiply = function (num1, num2) {
+    const len1 = num1.length, len2 = num2.length
+    const res = new Array(len1 + len2).fill(0)
+    for (let i = len1 - 1; i >= 0; i--){
+        for (let j = len2 - 1; j >= 0; j--){
+            const cur = num1[i] * num2[j]
+            const p1 = i + j, p2 = i + j + 1
+            const sum = cur + res[p2]
+            res[p1] += Math.floor(sum / 10)
+            res[p2] = sum % 10
+        }
+    }
+    if (res[0] == 0) res.shift()
+    return res.join('')
 };
+
+const num1 = '5', num2 = '6'
+console.log(multiply(num1, num2));
