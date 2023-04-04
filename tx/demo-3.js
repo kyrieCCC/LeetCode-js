c0788d4dddacc19aa97dc09d3d24fcf33c5bbf7c
kyrieCCC <1186624948@qq.com>
2023-04-04 23:00:04 +0800

    feat: 大数相减

--- tx/demo-3.js
+++ tx/demo-3.js
@@ -1,14 +1,29 @@
-const a = 2
-console.log(Math.pow(11, a));
+
 //大数相减
 const myFun = (num1, num2) => {
-    let count = 0
     const len1 = num1.length
     const len2 = num2.length
-    const maxLen = Math.max(len1, len2)
-    const arr1 = num1.toString().split("")
-    const arr2 = num2.toString().split("")
+    const arr1 = num1.toString().split("").map((item) => parseInt(item))
+    const arr2 = num2.toString().split("").map((item) => parseInt(item))
+    let i = arr1.length - 1, j = arr2.length - 1
+    const res = []
+    while (i >= 0 || j >= 0) {
+        if (arr1[i] >= arr2[j]) {
+            res.push(arr1[i] - arr2[j])
+        }
+        if (arr1[i] < arr2[j]) {
+            arr1[i - 1] -= 1
+            arr1[i] += 10
+            res.push(arr1[i] - arr2[j])
+        }
+        i--, j--
+    }
+    return res
 }
 
-const num1 = 22222222222222222222
-const num2 = 11111111111111111111
+const num1 = '22222222222222222222'
+const num2 = '11111111111111111111'
+console.log(num1.length
+);
+const res = myFun(num1, num2)
+console.log(res.length);
