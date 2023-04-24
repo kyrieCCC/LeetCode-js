84230f80e7e49b86209d89925df69aa89cf092ea
kyrieCCC <1186624948@qq.com>
2023-04-24 22:54:43 +0800

    feat: 326-1-1

---
+++ 300-400/326.isPowerOfThree.js
@@ -0,0 +1,24 @@
+// 326. 3 的幂
+// 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+// 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3 ^ x
+// 输入：n = 27
+// 输出：true
+
+var isPowerOfThree = function (n) { 
+    if (n === 1) {
+        return true
+    }
+    let val = 3
+    while (n >= val) {
+        if (n == val) {
+            return true
+        }
+        val = val * 3 
+    }
+    return false
+}
+
+const a1 = 5, a2 = 9, a3 = 27
+console.log(isPowerOfThree(a1));
+console.log(isPowerOfThree(a2));
+console.log(isPowerOfThree(a3));
