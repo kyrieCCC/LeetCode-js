194369591c1799f07c20cccb61993e6870b2e5ce
kyrieCCC <1186624948@qq.com>
2023-04-27 16:26:35 +0800

    feat: 342-1

---
+++ 300-400/342.isPowerOfFour.js
@@ -0,0 +1,27 @@
+// 342. 4的幂
+// 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+// 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+// 输入：n = 16
+// 输出：true
+
+var isPowerOfFour = function (n) { 
+    if (n == 1) {
+        return true
+    }
+    let val = 4
+    while (n >= val) {
+        if (n == val) {
+            return true
+        }
+        val = val * 4
+    }
+    return false
+}
+
+const a1 = 1
+const a2 = 5
+const a3 = 64
+
+console.log(isPowerOfFour(a1));
+console.log(isPowerOfFour(a2));
+console.log(isPowerOfFour(a3));
