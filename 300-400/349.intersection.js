2cc93d994248a92ed35dba933bbe44b4dc2858fd
kyrieCCC <1186624948@qq.com>
2023-05-07 20:39:51 +0800

    feat: 349-1

---
+++ 300-400/349.intersection.js
@@ -0,0 +1,15 @@
+// 349. 两个数组的交集
+// 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。
+// 输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+// 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+// 输出：[2]
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) { 
+
+}
