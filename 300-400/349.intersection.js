9a5f46b712af5a0e2831347df7f0b2bf63d0fdfc
kyrieCCC <1186624948@qq.com>
2023-05-07 20:40:45 +0800

    feat: 349-2

--- 300-400/349.intersection.js
+++ 300-400/349.intersection.js
@@ -11,5 +11,30 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) { 
+    const map = new Map()
+    const map2 = new Map()
+    const res1 = []
+    const res2 = []
 
+    for(let i = 0; i < nums1.length; i++){
+        map.set(nums1[i], map.get(nums1[i]) == undefined ? 1 : 1) 
+    }
+
+    for(let i = 0; i < nums2.length; i++){
+        if(map.has(nums2[i])){
+            res1.push(nums2[i])
+        }
+    }
+    
+    for(let i = 0; i < res1.length; i++){
+        if(map2.has(res1[i])){
+            continue
+        }
+        map2.set(res1[i], 1)
+        res2.push(res1[i])
+    }
+    return res2 
 }
+
+const nums1 = [1, 2, 2, 1], nums2 = [2, 2]
+console.log(intersection(nums1, nums2));
