ea93407b183c725ab792e2c002f40d6bd49ebbb7
kyrieCCC <1186624948@qq.com>
2023-05-09 21:52:30 +0800

    feat: 225-1-1

---
+++ 200-300/225.MyStack.js
@@ -0,0 +1,65 @@
+// 225. 用队列实现栈
+// 请你仅使用两个队列实现一个后入先出（LIFO）的栈，
+// 并支持普通栈的全部四种操作（push、top、pop 和 empty）
+
+// 实现 MyStack 类：
+// void push(int x) 将元素 x 压入栈顶。
+// int pop() 移除并返回栈顶元素。
+// int top() 返回栈顶元素。
+// boolean empty() 如果栈是空的，返回 true ；否则，返回 false
+
+// 输入：
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// 输出：
+// [null, null, null, 2, 2, false]
+
+var MyStack = function() {
+    this.queue = []
+    this._queue = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    while(this.queue.length > 1) {
+        this._queue.push(this.queue.shift())
+    }
+    const ans = this.queue.shift()
+    while(this._queue.length) {
+        this.queue.push(this._queue.shift())
+    }
+    return ans
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue.slice(-1)[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return !this.queue.length
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
