89a29692c51ecd0e20817d6b5dcca61e84d3d159
kyrieCCC <1186624948@qq.com>
2023-05-24 23:05:03 +0800

    feat: 704-1-2

--- 700-800/704.search.js
+++ 700-800/704.search.js
@@ -11,5 +11,21 @@
  * @return {number}
  */
 var search = function (nums, target) { 
-    
+    let left = 0, right = nums.length - 1
+    while (left <= right) {
+        let midIndex = Math.floor((right - left) / 2) + left
+        if (target == nums[midIndex]) {
+            return midIndex
+        }
+        if (target < nums[midIndex]) {
+            right = midIndex - 1
+        }
+        else {
+            left = midIndex + 1
+        }
+    }
+    return -1
 }
+
+const nums1 = [-1, 0, 3, 5, 9, 12], target1 = 9
+const nums2 = [-1, 0, 3, 5, 9, 12], target2 = 2
