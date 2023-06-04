f248ce80b77114061250ea95ccfa317f8d4964d8
kyrieCCC <1186624948@qq.com>
2023-06-04 15:12:43 +0800

    feat: 485-1-1

--- 400-500/485.findMaxConsecutiveOnes.js
+++ 400-500/485.findMaxConsecutiveOnes.js
@@ -11,5 +11,29 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) { 
-    
+    let max = 0
+    let i = nums.indexOf(1), j = nums.indexOf(1)
+    if (nums.length == 1 || i == -1) {
+        return nums[0]
+    }
+    while(j < nums.length) {
+        if(nums[j + 1] == 1) {
+            j++
+        } else {
+            max = Math.max(j - i + 1, max)
+            i = nums.indexOf(1, j + 1) 
+            j = nums.indexOf(1, j + 1)
+            if (i == -1) {
+                return max
+            }
+        }
+    }
 }
+
+const nums1 = [1, 1, 0, 1, 1, 1]
+const nums2 = [1, 0, 1, 1, 0, 1]
+const nums3 = [0, 0]
+
+console.log(findMaxConsecutiveOnes(nums1));
+console.log(findMaxConsecutiveOnes(nums2));
+console.log(findMaxConsecutiveOnes(nums3));
