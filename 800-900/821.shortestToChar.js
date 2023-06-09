ff62efcc32cf71aeb8b5a494cd64c0a25a05017d
kyrieCCC <1186624948@qq.com>
2023-06-09 18:02:59 +0800

    feat: 821-1-1

--- 800-900/821.shortestToChar.js
+++ 800-900/821.shortestToChar.js
@@ -16,6 +16,25 @@
  * @param {character} c
  * @return {number[]}
  */
-var shortestToChar = function (s, c) { 
-    
+var shortestToChar = function (s, c) {
+    const n = s.length;
+    const ans = new Array(n).fill(0);
+
+    for (let i = 0, idx = -n; i < n; ++i) {
+        if (s[i] === c) {
+            idx = i;
+        }
+        ans[i] = i - idx;
+    }
+
+    for (let i = n - 1, idx = 2 * n; i >= 0; --i) {
+        if (s[i] == c) {
+            idx = i;
+        }
+        ans[i] = Math.min(ans[i], idx - i);
+    }
+    return ans;
 }
+
+const s1 = "loveleetcode", c1 = "e"
+console.log(shortestToChar(s1, c1));
