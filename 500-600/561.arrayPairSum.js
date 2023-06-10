f26e0b365cac984fd0df612afa97f9995738121e
kyrieCCC <1186624948@qq.com>
2023-06-10 10:24:22 +0800

    feat: 561-1-1

--- 500-600/561.arrayPairSum.js
+++ 500-600/561.arrayPairSum.js
@@ -15,5 +15,14 @@
  * @return {number}
  */
 var arrayPairSum = function (nums) { 
-    
+    nums = nums.sort((a, b) => a - b)
+    let res = 0
+    for (let i = 0; i < nums.length; i += 2){
+        res += nums[i]
+    }
+    return res
 }
+const nums1 = [1, 4, 3, 2]
+const nums2 = [6, 2, 6, 5, 1, 2]
+console.log(arrayPairSum(nums1));
+console.log(arrayPairSum(nums2));
