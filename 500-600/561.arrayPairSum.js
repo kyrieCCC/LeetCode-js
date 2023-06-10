bb0e9742d8dfb44d79e65b001e22d0d2d90d2021
kyrieCCC <1186624948@qq.com>
2023-06-10 10:56:47 +0800

    feat: 561-2-2

--- 500-600/561.arrayPairSum.js
+++ 500-600/561.arrayPairSum.js
@@ -40,6 +40,29 @@ const arrayPairSum_plus = (nums) => {
 // sort排序，filter过滤奇数项，reduce计算结果
 // 最后输出值
 
+const arrayPairSum_fast = (nums) => {
+    const fast = (nums) => {
+        if (nums.length <= 1) {
+            return nums
+        }
+        const mid = Math.floor(nums.length / 2)
+        const midItem = nums.splice(mid, 1)[0]
+        const left = [], right = []
+        for (let i = 0; i < nums.length; i++) {
+            nums[i] < midItem ? left.push(nums[i]) : right.push(nums[i])
+        }
+        return fast(left).concat(midItem, fast(right))
+    }
+    const arr = fast(nums)
+    let res = 0
+    for (let i in arr) {
+        if (i % 2 == 0) {
+            res += arr[i]
+        }
+    }
+    return res
+}
+
 const nums1 = [1, 4, 3, 2]
 const nums2 = [6, 2, 6, 5, 1, 2]
 console.log(arrayPairSum(nums1));
@@ -47,3 +70,6 @@ console.log(arrayPairSum(nums2));
 console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
 console.log(arrayPairSum_plus(nums1));
 console.log(arrayPairSum_plus(nums2));
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
+console.log(arrayPairSum_fast(nums1));
+console.log(arrayPairSum_fast(nums2));
