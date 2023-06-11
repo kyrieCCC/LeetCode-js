e9ce0fa1bef5946e1c552743860a72d79b72d49f
kyrieCCC <1186624948@qq.com>
2023-06-11 21:38:48 +0800

    feat: 459-1

---
+++ 400-500/459.repeatedSubstringPattern.js
@@ -0,0 +1,15 @@
+// 459. 重复的子字符串
+// 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
+// 输入: s = "abab"
+// 输出: true
+// 解释: 可由子串 "ab" 重复两次构成
+// 输入: s = "aba"
+// 输出: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function (s) { 
+    
+}
