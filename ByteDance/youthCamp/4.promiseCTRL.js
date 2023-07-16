21a566b85a19bcb45a24f2b1ca9195c6fb256ca4
kyrieCCC <1186624948@qq.com>
2023-07-16 19:59:17 +0800

    feat: promise并发控制测试通过

--- ByteDance/youthCamp/4.promiseCTRL.js
+++ ByteDance/youthCamp/4.promiseCTRL.js
@@ -25,3 +25,12 @@ class PromiseCtrl {
     }
 }
 
+const promiseTest = new PromiseCtrl(1)
+const tasks = [
+    () => { console.log('promise1'); return new Promise(resolve => setTimeout(() => resolve('1'), 1000)) },
+    () => { console.log('promise2'); return new Promise(resolve => setTimeout(() => resolve('2'), 1000)) },
+    () => { console.log('promise3'); return new Promise(resolve => setTimeout(() => resolve('3'), 1000)) },
+    () => { console.log('promise4'); return new Promise(resolve => setTimeout(() => resolve('4'), 1000)) },
+    () => { console.log('promise5'); return new Promise(resolve => setTimeout(() => resolve('fn'), 1000)) },
+]
+const res = tasks.map((item) => promiseTest.add(item))
