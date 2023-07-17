97c389c21e726db218b95050c619dffad59c1a48
kyrieCCC <1186624948@qq.com>
2023-07-18 00:04:15 +0800

    feat: parseurl-add-arr

--- ByteDance/youthCamp/5.parseUrl.js
+++ ByteDance/youthCamp/5.parseUrl.js
@@ -9,20 +9,26 @@ const parseUrl = (url) => {
     for (let i = 0; i < queryItem.length; i++){
         const item = queryItem[i].split('=');
         if (Number(item[1])) {
-            res[item[0]] = Number(item[1]);
+            item[0] ? have(item[0], item[1], res) : res[item[0]] = Number(item[1]);
         }
         else if (item[1] == 'true' || item[1] == 'false') {
-            if (item[1] == 'true') res[item[0]] = true;
-            else res[item[0]] = false;
+            if (item[1] == 'true') item[0] ? have(item[0], item[1], res) : res[item[0]] = true;
+            else item[0] ? have(item[0], item[1], res) : res[item[0]] = false;
         }
         else if (item[1] == 'null') {
-            res[item[0]] = null;
+            item[0] ? have(item[0], item[1], res) : res[item[0]] = null;
         }
         else {
-            res[item[0]] = item[1]
+            item[0] ? have(item[0], item[1], res) : res[item[0]] = item[1]
         }
     }
     return res
+}
+
+const have = (key, value, obj) => {
+    const arr = [obj[key]];
+    arr.push(value);
+    obj[key] = arr;
 }
 
 const url1 = 'https://www.baidu.com/search?a=123&b=wlc&c=false&d=null&e=ph'
