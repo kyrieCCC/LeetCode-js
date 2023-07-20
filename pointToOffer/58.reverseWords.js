2373f9661903f88f4ab4b405fa098ffc63ec4e5d
kyrieCCC <1186624948@qq.com>
2023-07-20 21:51:31 +0800

    feat: 58-init

---
+++ pointToOffer/58.reverseWords.js
@@ -0,0 +1,20 @@
+// 剑指 Offer 58 - I. 翻转单词顺序
+// 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。
+// 例如输入字符串"I am a student. "，则输出"student. a am I"。
+// 输入: "the sky is blue"
+// 输出: "blue is sky the"
+// 输入: "  hello world!  "
+// 输出: "world! hello"
+// 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+
+}
+ 
+const s1 = "the sky is blue";
+const s2 = "  hello world!  ";
+const s3 = "a good   example";
