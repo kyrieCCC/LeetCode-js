b1e5e5493f0ec9e4d6fb051a50df6e706db16455
kyrieCCC <1186624948@qq.com>
2023-07-23 22:55:50 +0800

    feat: 50-1-1

--- pointToOffer/50.firstUniqChar.js
+++ pointToOffer/50.firstUniqChar.js
@@ -10,5 +10,13 @@
  * @return {character}
  */
 var firstUniqChar = function (s) { 
-    
+    const char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let pos = -1;
+    for(const item of char) {
+        // aabccdeff
+        if(s.includes(item) && s.indexOf(item) == s.lastIndexOf(item)){
+            pos = pos == -1 ? s.indexOf(item) : Math.min(s.indexOf(item), pos)
+        }
+    }
+    return pos = pos == -1 ? ' ' : s.charAt(pos)
 }
