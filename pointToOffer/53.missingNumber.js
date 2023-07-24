177a651b5e8e282d68fede820cca13422e849f21
kyrieCCC <1186624948@qq.com>
2023-07-24 22:22:54 +0800

    feat: 53-3-1

--- pointToOffer/53.missingNumber.js
+++ pointToOffer/53.missingNumber.js
@@ -48,6 +48,18 @@ const missingNumber_ans = (nums) => {
 // 记录每一个出现过的元素
 // 再来遍历元素找出缺失的值
 
+const missingNumber_max = (nums) => {
+    let xor = 0;
+    const n = nums.length + 1;
+    for (let i = 0; i < n - 1; i++) {
+        xor ^= nums[i];
+    }
+    for (let i = 0; i <= n - 1; i++) {
+        xor ^= i;
+    }
+    return xor;
+}
+
 const nums1 = [0, 1, 3]
 const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 9]
 console.log(missingNumber(nums1));
