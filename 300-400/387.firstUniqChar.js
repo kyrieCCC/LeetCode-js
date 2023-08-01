11c00dc6b4c6178f780e8131d7a938e593946d07
kyrieCCC <1186624948@qq.com>
2023-08-01 22:20:29 +0800

    feat: 387-1-1

--- 300-400/387.firstUniqChar.js
+++ 300-400/387.firstUniqChar.js
@@ -11,6 +11,22 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {
-    
+var firstUniqChar = function (s) {
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    map.has(s[i]) ? map.set(s[i], map.get(s[i]) + 1) : map.set(s[i], 1);
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
 };
+
+const s1 = "leetcode";
+const s2 = "loveleetcode";
+const s3 = "aabb";
+console.log(firstUniqChar(s1));
+console.log(firstUniqChar(s2));
+console.log(firstUniqChar(s3));
