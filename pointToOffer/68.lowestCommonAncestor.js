36819a5b66081872ece33efa50e045c35e9e763b
kyrieCCC <1186624948@qq.com>
2023-08-11 23:11:52 +0800

    feat: 68-1-1

--- pointToOffer/68.lowestCommonAncestor.js
+++ pointToOffer/68.lowestCommonAncestor.js
@@ -22,5 +22,16 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) { 
-    
+    let res;
+    const dfs = (root, p, q) => {
+        if (root === null) return false;
+        const lson = dfs(root.left, p, q);
+        const rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val === p.val || root.val === q.val) && (lson || rson))) {
+            ans = root;
+        } 
+        return lson || rson || (root.val === p.val || root.val === q.val);
+    }
+    dfs(root, p, q);
+    return res
 }
